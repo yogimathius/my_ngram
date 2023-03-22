9464f51b0e914d57701b03a747ba7acdf00d466d
jocelyn wilhelmsen <wilhelms_j@qwasar.io>
2023-03-22 23:56:12 +0000

    working

--- tester.c
+++ tester.c
@@ -1,6 +1,67 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+
+int my_strlen(char* str);
+int letter_occurrence(char* str, char letter);
+void my_ngram(char* str);
+
+int main(int argc, char **argv) {
+  if (argc == 3) {
+    strcat(argv[1], argv[2]);
+    my_ngram(argv[1]);
+  }
+  if (argc == 2) {
+    my_ngram(argv[1]);
+  }
+  
+  return 0;
+}
+
+int my_strlen(char* str) {
+    int i = 0;
+    while(str[i] != '\0') {
+        ++i;
+    }
+    return i;
+}
+
+int letter_occurrence(char* str, char letter) {
+    int count = 0;
+    for(int i = 0; i < my_strlen(str); ++i){
+        if(str[i] == letter) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+void my_ngram(char* str) {
+    int length = my_strlen(str);
+    char unique_arr[length];
+    int unique_count = 0;
+    bool been_counted;
+    for(int i = 0; i < length; ++i) {
+        int j = 0;
+        while(j <= unique_count){
+            if(str[i] == unique_arr[j]){
+                been_counted = true;
+                break;
+            }
+            else {
+                been_counted = false;
+            }
+            ++j;
+        }
+        if (been_counted == false) {
+            unique_arr[unique_count] = str[i];
+            ++unique_count;
+            printf("%c:%d\n", str[i], letter_occurrence(str, str[i]));
+        }
+            
+    }
+}
+
 /*
 struct key_value {
   int letter;
@@ -48,56 +109,4 @@ int main(int argc, char **argv) {
   return 0;
 }
 */
-int my_strlen(char* str) {
-    int i = 0;
-    while(str[i] != '\0') {
-        ++i;
-    }
-    return i;
-}
 
-int letter_occurrence(char* str, char letter) {
-    int count = 0;
-    for(int i = 0; i < my_strlen(str); ++i){
-        if(str[i] == letter) {
-            ++count;
-        }
-    }
-    return count;
-}
-
-void my_ngram(char* str) {
-    int length = my_strlen(str);
-    char unique_arr[length];
-    int unique_count = 0;
-    int counted = 0;
-    bool been_counted;
-    for(int i = 0; i < length; ++i) {
-        for(int j = 0; j < counted; ++j) {
-            if(str[i] == unique_arr[j]) {
-                been_counted = true;
-                break;
-            } 
-            else {
-                been_counted = false;
-                unique_arr[unique_count] = str[i];
-            }
-        }
-        if (been_counted == false) {
-            printf("%c:%d\n", str[i], letter_occurrence(str, str[i]));
-        }
-            
-    }
-}
-
-int main(int argc, char **argv) {
-  if (argc == 3) {
-    strcat(argv[1], argv[2]);
-    my_ngram(argv[1]);
-  }
-  if (argc == 2) {
-    my_ngram(argv[1]);
-  }
-  
-  return 0;
-}

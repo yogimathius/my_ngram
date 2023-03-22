18176020f24de269562f06e11d95933e94e74e55
jocelyn wilhelmsen <wilhelms_j@qwasar.io>
2023-03-22 23:18:36 +0000

    printing to many times

--- tester.c
+++ tester.c
@@ -56,27 +56,37 @@ int my_strlen(char* str) {
     return i;
 }
 
+int letter_occurrence(char* str, char letter) {
+    int count = 0;
+    for(int i = 0; i < my_strlen(str); ++i){
+        if(str[i] == letter) {
+            ++count;
+        }
+    }
+    return count;
+}
+
 void my_ngram(char* str) {
-    char unique_arr[] = {};
     int length = my_strlen(str);
+    char unique_arr[length];
+    int unique_count = 0;
     int counted = 0;
     bool been_counted;
     for(int i = 0; i < length; ++i) {
-        for(int j = 0; j < counted; ++j){
-        if(str[i] == unique_arr[j]) {
-            been_counted = true;
-        }
-        else {
-            for(int t = 0; t < length; ++t) {
-            if(str[i] == str[t]){
-                ++counted;
+        for(int j = 0; j < counted; ++j) {
+            if(str[i] == unique_arr[j]) {
+                been_counted = true;
+                break;
+            } 
+            else {
+                been_counted = false;
+                unique_arr[unique_count] = str[i];
             }
-            }
-            int count;
-            printf("%c:%d\n", str[i], count);
         }
-
+        if (been_counted == false) {
+            printf("%c:%d\n", str[i], letter_occurrence(str, str[i]));
         }
+            
     }
 }
 

5dda42e63450ae75c240130343154d7639516622
jocelyn wilhelmsen <wilhelms_j@qwasar.io>
2023-03-23 23:08:24 +0000

    working my_ngram

--- ngram_func.c
+++ ngram_func.c
@@ -61,7 +61,6 @@ void my_ngram(char* str) {
             
     }
 }
-
 /*
 struct key_value {
   int letter;
@@ -81,13 +80,13 @@ void my_ngram2(char *str) {
     bool letter_found = false;
     for (int j = 0; j < i; j++) {
       if (kvs[j].letter == letter) {
-        //printf("incrementing letter, %c\n", letter);
+        printf("incrementing letter, %c\n", letter);
         kvs[j].count++;
         letter_found = true;
       }
     }
     if (!letter_found) {
-      //printf("original letter, %c\n", letter);
+      printf("original letter, %c\n", letter);
       kvs[i].letter = letter;
       kvs[i].count = 1;
       letters_counted++;
@@ -97,5 +96,16 @@ void my_ngram2(char *str) {
   for (int i = 0; i < letters_counted; ++i) {
     printf("%c:%d\n", kvs[i].letter, kvs[i].count);
   }
-}
-*/
+}*/
+/*
+int main(int argc, char **argv) {
+  if (argc == 3) {
+    strcat(argv[1], argv[2]);
+    my_ngram2(argv[1]);
+  }
+  if (argc == 2) {
+    my_ngram2(argv[1]);
+  }
+  
+  return 0;
+}*/

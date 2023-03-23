01ab2a3b8414ac4e1ca4b803b7815f0e5544379e
jocelyn wilhelmsen <wilhelms_j@qwasar.io>
2023-03-23 20:10:47 +0000

    messed with my_ngram2 a little

--- ngram_func.c
+++ ngram_func.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
+char* sort_alphabeticaly(char* str, int length) {
+    char temp_letter;
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = i + 1; j < length; j++) {
+            if(str[i] > str[j]) {
+                temp_letter = str[i];
+                str[i] = str[j];
+                str[j] = temp_letter;
+            }
+        }
+    }
+    return str;
+}
+
 int my_strlen(char* str) {
     int i = 0;
     while(str[i] != '\0') {
@@ -20,19 +34,6 @@ int letter_occurrence(char* str, char letter) {
     return count;
 }
 
-char* sort_alphabeticaly(char* str, int length) {
-    char temp_letter;
-    for(int i = 0; i < length - 1; i++) {
-        for(int j = i + 1; j < length; j++) {
-            if(str[i] > str[j]) {
-                temp_letter = str[i];
-                str[i] = str[j];
-                str[j] = temp_letter;
-            }
-        }
-    }
-    return str;
-}
 
 void my_ngram(char* str) {
     int length = my_strlen(str);
@@ -60,3 +61,41 @@ void my_ngram(char* str) {
             
     }
 }
+
+/*
+struct key_value {
+  int letter;
+  int count;
+};
+
+void my_ngram2(char *str) {
+    int length = my_strlen(str);
+    str = sort_alphabeticaly(str, length);
+#define n strlen(str)
+
+  struct key_value kvs[n];
+
+  int letters_counted = 0;
+  for (int i = 0; i < n; ++i) {
+    char letter = str[i];
+    bool letter_found = false;
+    for (int j = 0; j < i; j++) {
+      if (kvs[j].letter == letter) {
+        //printf("incrementing letter, %c\n", letter);
+        kvs[j].count++;
+        letter_found = true;
+      }
+    }
+    if (!letter_found) {
+      //printf("original letter, %c\n", letter);
+      kvs[i].letter = letter;
+      kvs[i].count = 1;
+      letters_counted++;
+    }
+  }
+
+  for (int i = 0; i < letters_counted; ++i) {
+    printf("%c:%d\n", kvs[i].letter, kvs[i].count);
+  }
+}
+*/
